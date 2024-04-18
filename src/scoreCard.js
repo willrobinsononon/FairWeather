adfa38af1edaacca06001bb482d60c1993fdc52f
Will Robinson <willrobinsononon@yahoo.com>
2024-04-18 12:29:37 +0100

    first commit

---
+++ src/scoreCard.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export default function ScoreCard() {
+    return (
+        <div className = "score-card">
+            
+        </div>
+    )
+}

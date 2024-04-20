478df665be2590845f4d43bd149b2d20fa9617d6
Will Robinson <willrobinsononon@yahoo.com>
2024-04-20 17:41:17 +0100

    score list built, team picker module created

--- src/scoreCard.js
+++ src/scoreCard.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function ScoreCard() {
     return (
         <div className = "score-card">
-            
+            test
         </div>
     )
 }
